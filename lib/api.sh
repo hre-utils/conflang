6b64606cb01704bbf1ccc9e3b39de3d3145da7d6
Marcus Aurelius <marcus+git@aurelius.org>
2022-07-24 16:44:10 -0700

    Minor profiling, increase query speed
    Switched from a `while` loop with an increment to a `for` loop. Speeds
    up by a little bit.

--- lib/api.sh
+++ lib/api.sh
@@ -7,24 +7,27 @@
 #  _DATA_ROOT
 #  _DATA_$n
 
-declare -- RV
+declare -g RV
 
 function conf {
-   declare -g RV=$_DATA_ROOT
+   RV=$_DATA_ROOT
 
-   while [[ $# -gt 0 ]] ; do
+   for arg in "$@" ; do
       local -n d=$RV
 
+      # TODO: profiling
+      # If you're suuuuper sure you're not going to have any invalid selectors,
+      # taking this out shaves like 20% off the query time.
+      #
       # Test if the selector exists. If it's trying to query an index that's
       # *UNSET*, rather than just declared as an empty string, it explodes.
-      if [[ ! "${d[$1]+_}" ]] ; then
-         echo "selector '$1' not found." 1>&2
+      if [[ ! "${d[$arg]+_}" ]] ; then
+         echo "selector '$arg' not found." 1>&2
          exit -1
          # TODO: error reporting
          # Tracebacks would be A+ here.
       fi
 
-      declare -g RV="${d[$1]}"
-      shift && (( idx++ ))
+      RV="${d[$arg]}"
    done
 }
